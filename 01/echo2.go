b1a742626ff05f75e7303304489fcb2023d01c0b
ffffengfengfeng <1098865109@qq.com>
2020-07-22 10:40:18 +0800

    命令行输入

--- 01/echo2.go
+++ 01/echo2.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main() {
+	var result []string
+	for _, item := range os.Args[1:] {
+		result = append(result, item)
+	}
+	fmt.Println("参数列表: ", strings.Join(result, ","))
 }
